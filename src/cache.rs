abae852771db7f55b5b65a6ba5e3d6b27db92c2f
Yannick Funk <yannickfunk@yahoo.de>
2022-06-21 19:38:20 +0200

    Fix rambled file

--- src/cache.rs
+++ src/cache.rs
@@ -1,173 +1,163 @@
+use crate::db::Database;
+use crate::highlight::DATA;
 use crate::id::Id;
 use crate::{Entry, Error};
-use once_cell::sync::Lazy;
-use rusqlite::{params, Connection};
-use rusqlite_migration::{Migrations, M};
-use std::path::PathBuf;
+use axum::extract::Path;
+use lru::LruCache;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 use std::sync::{Arc, Mutex};
-use tokio::task::spawn_blocking;
 
-#[derive(Clone)]
-pub struct Database {
-    conn: Arc<Mutex<Connection>>,
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Key {
+    id: Id,
+    ext: String,
 }
 
-#[derive(Debug)]
-pub enum Open {
-    Memory,
-    Path(PathBuf),
+pub struct Inner {
+    cache: LruCache<Key, String>,
+    cached: HashMap<Id, HashSet<String>>,
 }
 
-static MIGRATIONS: Lazy<Migrations> = Lazy::new(|| {
-    Migrations::new(vec![M::up(include_str!("migrations/0001-up-initial.sql"))
-        .down(include_str!("migrations/0001-down-initial.sql"))])
-});
+type Cache = Arc<Mutex<Inner>>;
 
-impl Database {
-    pub fn new(method: Open) -> Result<Self, Error> {
-        tracing::debug!("opening {method:?}");
+impl Key {
+    pub fn new(id: Id, ext: String) -> Key {
+        Self { id, ext }
+    }
 
-        let mut conn = match method {
-            Open::Memory => Connection::open_in_memory()?,
-            Open::Path(path) => Connection::open(&path)?,
+    pub fn id(&self) -> String {
+        self.id.to_string()
+    }
+
+    pub fn extension(&self) -> String {
+        self.ext.clone()
+    }
+}
+
+impl TryFrom<Path<String>> for Key {
+    type Error = Error;
+
+    fn try_from(value: Path<String>) -> Result<Self, Self::Error> {
+        let (id, ext) = match value.split_once('.') {
+            None => (Id::try_from(value.as_str())?, "txt".to_string()),
+            Some((id, ext)) => (Id::try_from(id)?, ext.to_string()),
         };
 
-        MIGRATIONS.to_latest(&mut conn)?;
+        Ok(Self { id, ext })
+    }
+}
 
-        Ok(Self {
-            conn: Arc::new(Mutex::new(conn)),
-        })
+impl Inner {
+    pub fn new(size: usize) -> Self {
+        let cache = lru::LruCache::new(size);
+
+        Self {
+            cache,
+            cached: HashMap::new(),
+        }
     }
 
-    pub async fn insert(&self, id: Id, entry: Entry) -> Result<(), Error> {
-        let conn = self.conn.clone();
-        let id = id.as_u32();
-
-        spawn_blocking(move || match entry.expires {
-            None => conn.lock().unwrap().execute(
-                "INSERT INTO entries (id, text, burn_after_reading) VALUES (?1, ?2, ?3)",
-                params![id, entry.text, entry.burn_after_reading],
-            ),
-            Some(expires) => conn.lock().unwrap().execute(
-                "INSERT INTO entries (id, text, burn_after_reading, expires) VALUES (?1, ?2, ?3, datetime('now', ?4))",
-                params![
-                    id,
-                    entry.text,
-                    entry.burn_after_reading,
-                    format!("{expires} seconds")
-                ],
-            ),
-        })
-            .await??;
-
-        Ok(())
+    pub fn get<'a>(&'a mut self, k: &Key) -> Option<&'a String> {
+        self.cache.get(k)
     }
 
-    pub async fn get(&self, id: Id) -> Result<Entry, Error> {
-        let conn = self.conn.clone();
-        let id = id.as_u32();
-
-        let entry = spawn_blocking(move || {
-            conn.lock().unwrap().query_row(
-                "SELECT text, burn_after_reading FROM entries WHERE id=?1",
-                params![id],
-                |row| {
-                    Ok(Entry {
-                        text: row.get(0)?,
-                        extension: None,
-                        expires: None,
-                        burn_after_reading: row.get(1)?,
-                    })
-                },
-            )
-        })
-        .await??;
-
-        let conn = self.conn.clone();
-
-        if entry.burn_after_reading.unwrap_or(false) {
-            spawn_blocking(move || {
-                conn.lock()
-                    .unwrap()
-                    .execute("DELETE FROM entries WHERE id=?1", params![id])
-            })
-            .await??;
+    pub fn put(&mut self, k: Key, v: String) -> Option<String> {
+        if let Some(cached) = self.cached.get_mut(&k.id) {
+            if !cached.contains(&k.ext) {
+                cached.insert(k.ext.clone());
+            }
+        } else {
+            let mut set = HashSet::new();
+            set.insert(k.ext.clone());
+            self.cached.insert(k.id, set);
         }
 
-        Ok(entry)
+        self.cache.put(k, v)
     }
 
-    /// Remove all expired entries and return their `Id`s.
-    pub async fn purge(&self) -> Result<Vec<Id>, Error> {
-        tracing::debug!("purging");
+    pub fn remove(&mut self, id: Id) {
+        if let Some(exts) = self.cached.remove(&id) {
+            for ext in exts {
+                tracing::debug!("evicting {id:?}.{ext}");
+                self.cache.pop(&Key::new(id, ext));
+            }
+        }
+    }
+}
 
-        let conn = self.conn.clone();
+/// Cache layer combining database and cache access.
+#[derive(Clone)]
+pub struct Layer {
+    db: Database,
+    cache: Cache,
+}
 
-        spawn_blocking(move || {
-            let conn = conn.lock().unwrap();
+impl Layer {
+    pub fn new(db: Database, cache_size: usize) -> Self {
+        let cache = Arc::new(Mutex::new(Inner::new(cache_size)));
+        Self { db, cache }
+    }
 
-            let mut stmt =
-                conn.prepare("SELECT id FROM entries WHERE expires < datetime('now')")?;
+    /// Insert `entry` into the database.
+    pub async fn insert(&self, id: Id, entry: Entry) -> Result<(), Error> {
+        self.db.insert(id, entry).await
+    }
 
-            let ids = stmt
-                .query_map([], |row| Ok(Id::from(row.get::<_, u32>(0)?)))?
-                .collect::<Result<Vec<_>, _>>()?;
+    /// Look up or generate HTML formatted data. Return `None` if `key` is not found.
+    pub async fn get_formatted(&self, key: Key) -> Result<String, Error> {
+        if let Some(cached) = self.cache.lock().unwrap().get(&key) {
+            tracing::debug!(?key, "found cached item");
+            return Ok(cached.to_string());
+        }
 
-            conn.execute("DELETE FROM entries WHERE expires < datetime('now')", [])?;
+        let entry = self.db.get(key.id).await?;
+        let burn_after_reading = entry.burn_after_reading.unwrap_or(false);
+        let ext = key.ext.clone();
+        let formatted = tokio::task::spawn_blocking(move || DATA.highlight(&entry, &ext)).await??;
 
-            Ok(ids)
-        })
-        .await?
+        if !burn_after_reading {
+            tracing::debug!(?key, "cache item");
+            self.cache.lock().unwrap().put(key, formatted.clone());
+        }
+
+        Ok(formatted)
+    }
+
+    /// Get raw content for `id` or `None` if not found.
+    pub async fn get_raw(&self, id: Id) -> Result<String, Error> {
+        self.db.get(id).await.map(|e| e.text)
+    }
+
+    /// Purge expired items from database and cache.
+    pub async fn purge(&self) -> Result<(), Error> {
+        for id in self.db.purge().await? {
+            tracing::debug!(?id, "remove from cache");
+            self.cache.lock().unwrap().remove(id);
+        }
+        Ok(())
+    }
+}
+
+/// Purge `layer` every minute.
+pub async fn purge_periodically(layer: Layer) -> Result<(), Error> {
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(60));
+
+    loop {
+        interval.tick().await;
+        layer.purge().await?;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[tokio::test]
-    async fn insert() -> Result<(), Box<dyn std::error::Error>> {
-        let db = Database::new(Open::Memory)?;
-
-        let entry = Entry {
-            text: "hello world".to_string(),
-            extension: None,
-            expires: None,
-            burn_after_reading: None,
-        };
-
-        let id = Id::from(1234);
-        db.insert(id, entry).await?;
-
-        let entry = db.get(id).await?;
-        assert_eq!(entry.text, "hello world");
-
-        let result = db.get(Id::from(5678)).await;
-        assert!(result.is_err());
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn burn_after_reading() -> Result<(), Box<dyn std::error::Error>> {
-        let db = Database::new(Open::Memory)?;
-        let entry = Entry {
-            text: "hello world".to_string(),
-            extension: None,
-            expires: None,
-            burn_after_reading: Some(true),
-        };
-        let id = Id::from(1234);
-        db.insert(id, entry).await?;
-        assert!(db.get(id).await.is_ok());
-        assert!(db.get(id).await.is_err());
-
-        Ok(())
-    }
+    use crate::db;
 
     #[tokio::test]
     async fn expired_is_purged() -> Result<(), Box<dyn std::error::Error>> {
-        let db = Database::new(Open::Memory)?;
+        let db = Database::new(db::Open::Memory)?;
+        let layer = Layer::new(db, 128);
 
         let entry = Entry {
             text: "hello world".to_string(),
@@ -177,12 +167,14 @@ mod tests {
         };
 
         let id = Id::from(1234);
-        db.insert(id, entry).await?;
-        assert!(db.get(id).await.is_ok());
+        let key = Key::new(id, "rs".to_string());
+        layer.insert(id, entry).await?;
+        assert!(layer.get_formatted(key.clone()).await.is_ok());
 
-        tokio::time::sleep(std::time::Duration::from_millis(2000)).await;
-        db.purge().await?;
-        assert!(db.get(id).await.is_err());
+        tokio::time::sleep(std::time::Duration::from_millis(1500)).await;
+        layer.purge().await?;
+        assert!(layer.db.get(id).await.is_err());
+        assert!(layer.get_formatted(key).await.is_err());
 
         Ok(())
     }

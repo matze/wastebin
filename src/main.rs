8d55c812ba663708c30ea6b6132cf661f5b11be9
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2022-12-27 09:30:37 +0100

    Fix #13: drop anyhow
    We now just define a few custom error values for the parsing part and
    can thus just drop `.with_context()` calls. However, we have to adjust
    printing to avoid seeing the innards of the errors.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use crate::db::Database;
-use anyhow::{Context, Result};
 use axum::extract::DefaultBodyLimit;
 use axum::http::StatusCode;
 use axum::{Router, Server};
 use once_cell::sync::Lazy;
 use std::env::{self, VarError};
-use std::io;
 use std::net::SocketAddr;
-use std::num::{NonZeroUsize, TryFromIntError};
+use std::num::{NonZeroUsize, ParseIntError, TryFromIntError};
 use std::path::PathBuf;
+use std::process::ExitCode;
 use std::time::Duration;
 use tower_http::compression::CompressionLayer;
 use tower_http::timeout::TimeoutLayer;
@@ -27,6 +26,11 @@ pub static TITLE: Lazy<String> =
     Lazy::new(|| env::var("WASTEBIN_TITLE").unwrap_or_else(|_| "wastebin".to_string()));
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const VAR_ADDRESS_PORT: &str = "WASTEBIN_ADDRESS_PORT";
+const VAR_CACHE_SIZE: &str = "WASTEBIN_CACHE_SIZE";
+const VAR_DATABASE_PATH: &str = "WASTEBIN_DATABASE_PATH";
+const VAR_MAX_BODY_SIZE: &str = "WASTEBIN_MAX_BODY_SIZE";
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -52,6 +56,18 @@ pub enum Error {
     SyntaxParsing(#[from] syntect::parsing::ParsingError),
     #[error("time formatting error: {0}")]
     TimeFormatting(#[from] time::error::Format),
+}
+
+#[derive(thiserror::Error, Debug)]
+enum EnvError {
+    #[error("failed to parse {VAR_CACHE_SIZE}, expected number of elements: {0}")]
+    CacheSize(ParseIntError),
+    #[error("failed to parse {VAR_DATABASE_PATH}, contains non-Unicode data")]
+    DatabasePath,
+    #[error("failed to parse {VAR_MAX_BODY_SIZE}, expected number of bytes: {0}")]
+    MaxBodySize(ParseIntError),
+    #[error("failed to parse {VAR_ADDRESS_PORT}, expected `host:port`")]
+    AddressPort,
 }
 
 impl From<Error> for StatusCode {
@@ -85,21 +101,12 @@ pub(crate) fn make_app(max_body_size: usize) -> Router<cache::Layer> {
         .layer(DefaultBodyLimit::max(max_body_size))
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    const VAR_DATABASE_PATH: &str = "WASTEBIN_DATABASE_PATH";
-    const VAR_CACHE_SIZE: &str = "WASTEBIN_CACHE_SIZE";
-    const VAR_ADDRESS_PORT: &str = "WASTEBIN_ADDRESS_PORT";
-    const VAR_MAX_BODY_SIZE: &str = "WASTEBIN_MAX_BODY_SIZE";
-
+async fn start() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
     let database = match env::var(VAR_DATABASE_PATH) {
         Ok(path) => Ok(Database::new(db::Open::Path(PathBuf::from(path)))?),
-        Err(VarError::NotUnicode(_)) => Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            format!("{VAR_DATABASE_PATH} contains non-unicode data"),
-        )),
+        Err(VarError::NotUnicode(_)) => Err(EnvError::DatabasePath),
         Err(VarError::NotPresent) => Ok(Database::new(db::Open::Memory)?),
     }?;
 
@@ -108,18 +115,18 @@ async fn main() -> Result<()> {
             |_| Ok(NonZeroUsize::new(128).unwrap()),
             |s| s.parse::<NonZeroUsize>(),
         )
-        .with_context(|| format!("failed to parse {VAR_CACHE_SIZE}, expect number of elements"))?;
+        .map_err(EnvError::CacheSize)?;
 
     let cache_layer = cache::Layer::new(database, cache_size);
 
     let addr: SocketAddr = env::var(VAR_ADDRESS_PORT)
         .unwrap_or_else(|_| "0.0.0.0:8088".to_string())
         .parse()
-        .with_context(|| format!("failed to parse {VAR_ADDRESS_PORT}, expect `host:port`"))?;
+        .map_err(|_| EnvError::AddressPort)?;
 
     let max_body_size = env::var(VAR_MAX_BODY_SIZE)
         .map_or_else(|_| Ok(1024 * 1024), |s| s.parse::<usize>())
-        .with_context(|| format!("failed to parse {VAR_MAX_BODY_SIZE}, expect number of bytes"))?;
+        .map_err(EnvError::MaxBodySize)?;
 
     tracing::debug!("serving on {addr}");
     tracing::debug!("caching {cache_size} paste highlights");
@@ -145,4 +152,15 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    match start().await {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("Error: {err}");
+            ExitCode::SUCCESS
+        }
+    }
 }

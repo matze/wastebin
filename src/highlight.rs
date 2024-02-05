a546ae8c704a31e2bb4e289d8fff081138dad7cd
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2024-02-05 22:54:54 +0100

    Make clippy happy

--- src/highlight.rs
+++ src/highlight.rs
@@ -111,7 +111,7 @@ fn highlight(source: &str, ext: &str) -> Result<String, Error> {
             r#"<tr><td class="line-number" id="L{line_number}"><a href=#L{line_number}>{line_number:>4}</a></td>"#
         );
         html.push_str(&line_number);
-        html.push_str(&r#"<td class="line">"#);
+        html.push_str(r#"<td class="line">"#);
 
         if delta < 0 {
             html.push_str(&"<span>".repeat(delta.abs().try_into()?));

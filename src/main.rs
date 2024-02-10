1508ccc072a7f52a5d4c68e029a43ba8d1ce9ae1
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2024-02-10 17:22:34 +0100

    Fix pedantic clippy complaint

--- src/main.rs
+++ src/main.rs
@@ -71,8 +71,8 @@ async fn shutdown_signal() {
     let terminate = std::future::pending::<()>();
 
     tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
+        () = ctrl_c => {},
+        () = terminate => {},
     }
 
     tracing::info!("received signal, exiting ...");

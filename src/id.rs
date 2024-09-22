07e646b3dc14fb63b1f294b0bcdac547b89d0b16
Christian Göttsche <cgzones@googlemail.com>
2024-09-22 11:53:41 +0200

    Declare data table const instead of static

--- src/id.rs
+++ src/id.rs
@@ -3,7 +3,7 @@ use crate::errors::Error;
 use std::fmt;
 use std::str::FromStr;
 
-static CHAR_TABLE: &[char; 64] = &[
+const CHAR_TABLE: &[char; 64] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',

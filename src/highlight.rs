c8585a5b7dd73a5d7802ab4ffcbf25f759455c64
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-03-07 10:31:12 +0100

    Insert anchor tags in Markdown files
    Vendor some syntect code to modify the way code is rendered.

--- src/highlight.rs
+++ src/highlight.rs
@@ -1,8 +1,12 @@
 use crate::db::read::Data;
 use crate::errors::Error;
 use std::cmp::Ordering;
+use std::fmt::Write;
 use syntect::html::{ClassStyle, line_tokens_to_classed_spans};
-use syntect::parsing::{ParseState, ScopeStack, SyntaxReference, SyntaxSet};
+use syntect::parsing::{
+    BasicScopeStackOp, ParseState, SCOPE_REPO, Scope, ScopeStack, ScopeStackOp, SyntaxReference,
+    SyntaxSet,
+};
 use syntect::util::LinesWithEndings;
 
 const HIGHLIGHT_LINE_LENGTH_CUTOFF: usize = 2048;
@@ -46,6 +50,132 @@ impl Default for Highlighter {
     }
 }
 
+/// Escape HTML tags in `s` and write output to `buf`.
+fn escape(s: &str, buf: &mut String) -> std::fmt::Result {
+    // Because the internet is always right, turns out there's not that many
+    // characters to escape: http://stackoverflow.com/questions/7381974
+    let pile_o_bits = s;
+    let mut last = 0;
+    for (i, ch) in s.bytes().enumerate() {
+        match ch as char {
+            '<' | '>' | '&' | '\'' | '"' => {
+                buf.write_str(&pile_o_bits[last..i])?;
+                let s = match ch as char {
+                    '>' => "&gt;",
+                    '<' => "&lt;",
+                    '&' => "&amp;",
+                    '\'' => "&#39;",
+                    '"' => "&quot;",
+                    _ => unreachable!(),
+                };
+                buf.write_str(s)?;
+                last = i + 1;
+            }
+            _ => {}
+        }
+    }
+
+    if last < s.len() {
+        buf.write_str(&pile_o_bits[last..])?;
+    }
+
+    Ok(())
+}
+
+/// Transform `scope` atoms to CSS style classes and write output to `s`.
+fn scope_to_classes(s: &mut String, scope: Scope, style: ClassStyle) {
+    let repo = SCOPE_REPO.lock().expect("lock");
+    for i in 0..(scope.len()) {
+        let atom = scope.atom_at(i as usize);
+        let atom_s = repo.atom_str(atom);
+        if i != 0 {
+            s.push(' ');
+        }
+        match style {
+            ClassStyle::Spaced => {}
+            ClassStyle::SpacedPrefixed { prefix } => {
+                s.push_str(prefix);
+            }
+            _ => todo!("new variant"),
+        }
+        s.push_str(atom_s);
+    }
+}
+
+/// Return `true` if `scope` will be used to render a Markdown link.
+fn is_markdown_link(scope: Scope) -> bool {
+    let repo = SCOPE_REPO.lock().expect("lock");
+
+    (0..scope.len()).all(|index| {
+        let atom = repo.atom_str(scope.atom_at(index as usize));
+        atom == "markup" || atom == "underline" || atom == "link" || atom == "markdown"
+    })
+}
+
+/// Modified version of [`syntect::html::line_tokens_to_classed_spans`] that outputs HTML anchors
+/// for Markdown links.
+fn line_tokens_to_classed_spans_md(
+    line: &str,
+    ops: &[(usize, ScopeStackOp)],
+    style: ClassStyle,
+    stack: &mut ScopeStack,
+) -> Result<(String, isize), syntect::Error> {
+    let mut s = String::with_capacity(line.len() + ops.len() * 8); // a guess
+    let mut cur_index = 0;
+    let mut span_delta = 0;
+
+    let mut span_empty = false;
+    let mut span_start = 0;
+    let mut handling_link = false;
+
+    for &(i, ref op) in ops {
+        if i > cur_index {
+            span_empty = false;
+
+            if handling_link {
+                // Insert href and close attribute ...
+                escape(&line[cur_index..i], &mut s)?;
+                s.push_str(r#"">"#);
+                escape(&line[cur_index..i], &mut s)?;
+            } else {
+                escape(&line[cur_index..i], &mut s)?;
+            }
+            cur_index = i;
+        }
+        stack.apply_with_hook(op, |basic_op, _| match basic_op {
+            BasicScopeStackOp::Push(scope) => {
+                span_start = s.len();
+                span_empty = true;
+                s.push_str("<span class=\"");
+                scope_to_classes(&mut s, scope, style);
+                s.push_str("\">");
+                span_delta += 1;
+
+                if is_markdown_link(scope) {
+                    s.push_str(r#"<a href=""#);
+                    handling_link = true;
+                }
+            }
+            BasicScopeStackOp::Pop => {
+                if span_empty {
+                    s.truncate(span_start);
+                } else {
+                    s.push_str("</span>");
+                }
+                span_delta -= 1;
+                span_empty = false;
+
+                if handling_link {
+                    s.push_str("</a>");
+                    handling_link = false;
+                }
+            }
+        })?;
+    }
+    escape(&line[cur_index..line.len()], &mut s)?;
+    Ok((s, span_delta))
+}
+
 impl Highlighter {
     fn highlight_inner(&self, source: &str, ext: &str) -> Result<String, Error> {
         let syntax_ref = self
@@ -57,6 +187,7 @@ impl Highlighter {
                     .expect("finding txt syntax")
             });
 
+        let is_markdown = syntax_ref.name == "Markdown";
         let mut parse_state = ParseState::new(syntax_ref);
         let mut html = String::from("<table><tbody>");
         let mut scope_stack = ScopeStack::new();
@@ -66,12 +197,22 @@ impl Highlighter {
                 (line.to_string(), 0)
             } else {
                 let parsed = parse_state.parse_line(line, &self.syntax_set)?;
-                line_tokens_to_classed_spans(
-                    line,
-                    parsed.as_slice(),
-                    ClassStyle::Spaced,
-                    &mut scope_stack,
-                )?
+
+                if is_markdown {
+                    line_tokens_to_classed_spans_md(
+                        line,
+                        parsed.as_slice(),
+                        ClassStyle::Spaced,
+                        &mut scope_stack,
+                    )?
+                } else {
+                    line_tokens_to_classed_spans(
+                        line,
+                        parsed.as_slice(),
+                        ClassStyle::Spaced,
+                        &mut scope_stack,
+                    )?
+                }
             };
 
             line_number += 1;

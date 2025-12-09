3fb5bf3d84aecfb564ea360c3482f32fd9cc5bd4
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2025-12-09 19:56:29 +0100

    Update dependencies

--- crates/wastebin_server/src/main.rs
+++ crates/wastebin_server/src/main.rs
@@ -208,7 +208,10 @@ fn make_app(state: AppState, timeout: Duration, max_body_size: usize) -> Router 
                 .layer(DefaultBodyLimit::max(max_body_size))
                 .layer(CompressionLayer::new())
                 .layer(TraceLayer::new_for_http())
-                .layer(TimeoutLayer::new(timeout))
+                .layer(TimeoutLayer::with_status_code(
+                    StatusCode::REQUEST_TIMEOUT,
+                    timeout,
+                ))
                 .layer(from_fn_with_state(state.clone(), handle_service_errors))
                 .layer(from_fn(security_headers_layer)),
         )
